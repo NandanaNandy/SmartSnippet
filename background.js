74bcc1ca278665f30d0b2bee69b3f135424f7c4b
muralidharan-1708 <muralidharan1708@gmail.com>
2025-03-18 14:00:06 +0530

    Updated changes

--- background.js
+++ background.js
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
 chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.sendMessage(tab.id, { action: "toggle" });
 });
 
 // Add any additional background tasks here
-=======
-chrome.runtime.onInstalled.addListener(() => {
-    console.log("GitHub to Llama Extension Installed!");
-});
->>>>>>> 3a31fc3c326b82b905c6fe214379184f5000d52d
